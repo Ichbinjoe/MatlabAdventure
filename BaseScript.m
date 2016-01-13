656b6f7503bedee54ae654ed89bd21d1e6abea23
Joseph Hirschfeld <joe@ibj.io>
2016-01-13 16:45:10 -0500

    Add column definitions

--- BaseScript.m
+++ BaseScript.m
@@ -4,14 +4,28 @@ figure;imshow([World{1,:};World{2,:};World{3,:};World{4,:};World{5,:};World{6,:}
 warning('off','all'); % turns off all warning messages
 % Definitions
 
+% Type definitions
 PLAYER = 1;
 MONSTER = 2;
 SUPERMONSTER = 3;
 DOOR = 4;
-HEALTH = 5;
+HEALTHBOOST = 5;
 SWORD = 6;
 SHIELD = 7;
 BOOT = 8;
+
+% Column Definitions
+
+TYPE = 1;
+X_COL = 2;
+Y_COL = 3;
+ATTACK_COL = 4;
+ATTACK_BOOST_COL = 5;
+DEFENSE_COL = 6;
+DEFENSE_BOOST_COL = 7;
+HEALTH_COL = 8;
+SPEED_COL = 9;
+SPEED_BOOST_COL = 10;
 
 % Init
 
