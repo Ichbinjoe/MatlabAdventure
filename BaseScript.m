2fbcb30ffb94fb7176fd3b9761a91e7f11dca082
Joseph Hirschfeld <joe@ibj.io>
2016-01-13 16:34:47 -0500

    Add sections to base script

--- BaseScript.m
+++ BaseScript.m
@@ -2,3 +2,27 @@
 load Adventure   % Loads the board (World - 10x10 cell array) along with a number of different image, shown below.
 figure;imshow([World{1,:};World{2,:};World{3,:};World{4,:};World{5,:};World{6,:};World{7,:};World{8,:};World{9,:};World{10,:}]); %display board
 warning('off','all'); % turns off all warning messages
+% Definitions
+
+PLAYER = 1;
+MONSTER = 2;
+SUPERMONSTER = 3;
+DOOR = 4;
+HEALTH = 5;
+SWORD = 6;
+SHIELD = 7;
+BOOT = 8;
+
+% Init
+
+% Start Play
+
+% Movement
+
+% Collision
+
+% Combat Start
+
+% Combat End
+
+% End Play
